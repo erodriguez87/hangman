3e5682ad4ac77b5f0ffb602a814b4e756d5552b9
Eddie Rodriguez <erodriguez87@gmail.com>
2018-02-24 22:50:18 -0500

    bootstrapping this thing

---
+++ assets/javascript/game.js
@@ -0,0 +1,109 @@
+// Creates an array of nba players last names
+players = ["james", "jordan","bryant","wade","oneal","shaq", "westbrook","curry","harden"];
+
+// Creating variables to hold the number of wins, losses, tries left, and user guesses
+var wins = 0;
+var losses = 0;
+var tries = 12; 
+var guesses = [];
+
+// Creating variables that I am using for arrays of letters, line after that splits the variable into an array of letters...
+var player = players[Math.floor(Math.random() * players.length)];
+  playerArray = player.split('');
+var playerLength = playerArray.length
+var playerEmpty = [];
+console.log(player);
+console.log(playerArray)
+var j = 0
+var win = true
+
+function resetGame(){
+    j = 0;
+    tries = 12;
+    guesses = [];
+
+    player = players[Math.floor(Math.random() * players.length)];
+    playerArray = player.split('');
+    playerLength = playerArray.length
+    playerEmpty = [];
+
+    win = true;
+    emptySet();
+  }
+
+// Sets up the empty array that is used as the base for hangman
+function emptySet(){
+  for (var cnt = 0; cnt < playerLength; cnt++) {
+        playerEmpty[cnt] = "_ ";
+      }
+}
+
+function winCondition(){
+  for (var i = 0; i < playerLength; i++){
+    if (playerEmpty[i] === playerArray[i]) {
+        j++;
+        console.log('at least one letter matches') 
+        if (j===playerLength){
+          wins++;
+          console.log('victory, they all match')
+          resetGame();
+        }
+    } else {
+        j = 0;
+        console.log('no luck')
+        win = false;
+      }
+    }   
+  }
+
+  function lossCondition(){
+    if ((tries <= 0) && (win == false)) {
+      losses++;
+      resetGame();
+    }
+  }
+
+emptySet();
+// This function is run whenever the user presses a key. it stores their guess
+document.onkeyup = function(event) {
+  var userGuess = event.key;
+  if ((tries >= 0) && (playerEmpty != playerArray)) {
+
+    // loop through the empty array and if the guess matches add them to the same pos of empty array
+      for (var cnt2 = 0; cnt2 < playerLength; cnt2++) {
+        if ((tries >= 0) && (userGuess === playerArray[cnt2])) {
+          playerEmpty[cnt2] = userGuess;
+          console.log("you found a letter!")
+          winCondition();
+          lossCondition();
+        // check if the guess has already been guessed, if it has exit, if not reduce the number of available tries
+        }
+      } 
+    // check if the letter has already been guessed, if it has exit, if it hasn't then add the guess to the guess array and reduce the number of available tries
+      for (cnt3 = 0; cnt3 < playerLength; cnt3++) {
+        if (userGuess === guesses[cnt3]){
+          console.log("you already guessed that letter")
+          return
+        } else {
+          console.log("that letter isn't there")
+          lossCondition();
+        }
+      }
+      guesses.push(userGuess);
+      tries--;
+      lossCondition();
+  } 
+
+
+    // HTML for the user who can't use console.log
+    var html =
+      "<p>Hint: " + playerEmpty + "</p>" +          
+      "<p>You chose: " + userGuess + "</p>" +
+      "<p>wins: " + wins + "</p>" +
+      "<p>losses: " + losses + "</p>" +
+      "<p>Guesses left: " + tries + "</p>" +
+      "<p>Your guesses so far: " + guesses + "</p>";
+
+    // Set the inner HTML contents of the #game div to our html string
+    document.querySelector("#game").innerHTML = html;
+  }
